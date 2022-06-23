635ad076228fb17c83ecabe8ef3448ef959aa90d
AlirezaBabaei <99098510+AlirezaBabaeii@users.noreply.github.com>
2022-06-24 01:14:39 +0430

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
-
+/* eslint-disable @next/next/inline-script-id */
+import "../styles/globals.css";
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-RRW89BKSEL"}
+      />
+      <Script strategy="lazyOnload">{`
+      console.log('test')
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+           
+             gtag('config', 'G-RRW89BKSEL');
+                `}</Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
